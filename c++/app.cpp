7fae8e64261f16cf75824e30906bda11acab70e7
Shengzhe Chen <shengzhe.chen@gmail.com>
2020-12-31 00:19:02 -0800

    [c++]add main program

---
+++ c++/app.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+	cout << "Design Pattern Examples..." << endl;
+	cout << "1. Creational - Factory Method" << endl;
+	cout << "Choose the example aboove(Q or q to abort): " << endl;
+	
+	char input = '';
+	cin >> input;
+	while (tolower(input) != 'q') {
+		int _case = input - '0';
+		switch (_case) {
+			case 0:
+				break;
+			case 1:
+				break;
+			default:
+				cout << "Please enter a number shown above" << endl;
+				continue;
+		}
+
+	}
+
+	return 0;
+}
